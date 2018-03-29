6f6ec49d5ffb06bc9ec0130dcc146e9c4c81e7a4
andreearaduc <35446521+andreearaduc@users.noreply.github.com>
2018-03-29 09:10:29 +0300

    Update NDG.cpp

--- NDG.cpp
+++ NDG.cpp
@@ -1,29 +1,48 @@
 
 /*-pt i = 2 la n executa
     -determina tata = [i/2]
-    -atata timp v[tata] > v[i] si tata >= 1
+    -atata timp v[tata] > v54[i] si tata >= 1
         -interschimb v[tata] si v[i]
         -fiu = tata
         -tata = [fiu/2]
 */
 #include<iostream>
+
 using namespace std;
+
+void heap(int *v, int n)
+{
+for(int i=2;i<=n;i++)
+{
+int tata=i/2;
+int fiu=i;
+while(v[tata]>v[i]&&tata>=1)
+{
+int aux;
+aux=v[tata];
+v[tata]=v[i];
+v[i]=aux;
+
+fiu = tata;
+tata=fiu/2;
+} } }
+
 int n, tata, fiu;
+
 int main ()
+{
 cout<<"n= ";
 cin>>n;
 int v[n+1];
-int i;
-for (i=2; i<=n;  i++)
-{ tata=i/2;
-
-    while (v[tata] > v[i] && tata>=1)
-    {
-        int aux;
-
-        aux=tata;
-        tata=fiu;
-        fiu=aux;
-
-    }
+for(int i=1;i<=n;i++)
+{
+    cin>>v[i];
 }
+heap(v,n);
+for(int i=1;i<=n;i++)
+{
+    cout<<v[i]<<" ";
+}
+return 0;
+}
+`
